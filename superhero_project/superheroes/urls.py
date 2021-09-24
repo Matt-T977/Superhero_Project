93864ad3cf4a14a2451d46207db516ce59a92bd3
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-09-24 14:03:08 -0500

    Created the create function and started the ground work for the update function for the web application
    Created the create function and started the ground work for the update function for the web application
    
    Also did work on the html display templates and a few bug fixes.
    
    Current issue is running into a display issue for the update function.

--- superhero_project/superheroes/urls.py
+++ superhero_project/superheroes/urls.py
@@ -4,7 +4,10 @@ from django.urls import path
 from . import views
 
 
-app_name = 'Superheroes'
+app_name = 'superheroes'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:hero_id>/', views.detail, name='detail'),
+    path('new/', views.create, name='create'),
+    path('update/<int:hero_id>/', views.update, name='update'),
 ]
--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -1,7 +1,47 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Superhero
 
 
 # Create your views here.
 def index(request):
-    return render(request, "superheroes/index.html")
+    all_heroes = Superhero.objects.all()
+    context = {
+        'all_heroes' : all_heroes,
+    }
+    return render(request, "superheroes/index.html", context)
+
+def detail(request, hero_id):
+    single_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'single_hero' : single_hero,
+    }
+    return render(request, 'superheroes/detail.html', context)
+
+def create(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        catch_phrase = request.POST.get('catch_phrase')
+        primary_ability = request.POST.get('primary_ability')
+        secondary_ability = request.POST.get('secondary_ability')
+        new_hero = Superhero(name=name, alter_ego=alter_ego, catch_phrase=catch_phrase, primary_ability=primary_ability, secondary_ability=secondary_ability)
+        new_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
+
+def update(request, hero_id):
+    single_hero = Superhero.objects.get(pk=hero_id)
+    if request.method == "POST":
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        catch_phrase = request.POST.get('catch_phrase')
+        primary_ability = request.POST.get('primary_ability')
+        secondary_ability = request.POST.get('secondary_ability')
+        single_hero = single_hero(name=name, alter_ego=alter_ego, catch_phrase=catch_phrase, primary_ability=primary_ability, secondary_ability=secondary_ability)
+        single_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else: 
+        return render(request, 'superheroes/update.html')
